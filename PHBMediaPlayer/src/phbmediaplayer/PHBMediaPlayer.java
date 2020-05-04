27c2e5a63b4a92d23cf3c7d93dfae0454aae3c90
Fariq Fadillah <fariq.fgi@gmail.com>
2020-05-04 22:29:05 +0700

    media player

---
+++ PHBMediaPlayer/src/phbmediaplayer/PHBMediaPlayer.java
@@ -0,0 +1,34 @@
+package phbmediaplayer;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author ASUS X441N
+ */
+public class PHBMediaPlayer extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("PHB Media Player");
+        scene.setOnMouseClicked((MouseEvent event) -> {
+            if(event.getClickCount() == 2 ) {
+                stage.setFullScreen(true);
+            }
+        });
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
